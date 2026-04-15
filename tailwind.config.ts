ab3bf268d677bdbca2ebfb04e62dc44fb7a6dfc5
agareaditya-8055 <adityaagare8055@gmail.com>
2026-04-15 22:50:14 +0530

    Fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'tailwindcss';
-
+import preset from '@axiomui/tokens/tailwind.preset'
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  presets: [preset],
+  content: [
+    './index.html', 
+    './src/**/*.{ts,tsx}',  
+    "./node_modules/@axiomui/components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
